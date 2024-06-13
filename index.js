7791a8ae5621c60ac8ca8a300e8d49825ce3158d
fbeniscellip <fr.beniscelli@duocuc.cl>
2024-06-12 23:00:55 -0400

    aksjdkas

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const cors = require("cors");
 const puerto = 2006;
 //----------------------------------------------------
 
-const { getVehiculos , getVehiculosById } = require("./Controllers/vehiculos_const")
+const { getVehiculos , getVehiculosById } = require("./Controllers/vehiculos_const");
+const { vehiculos } = require("./vehiculos");
 
 app.use(cors());
 
@@ -19,4 +20,49 @@ app.get("/:id" , getVehiculosById)
 app.listen( puerto , ()=> {
     
     console.log(`Servidor arriba ok en el puerto ${puerto}`);
+});
+
+function getVehiculos(done) {
+
+    const result = fetch("https://api-vehiculos-lj3j.onrender.com/");
+
+    result
+        .then(response => response.json())
+        .then(data => {
+            done(data)
+        });
+
+}
+
+getVehiculos(data => {
+
+    data.result.array.forEach(vehiculos => {
+
+        const article = document.createRange().createContextualFragment(/*html*/`
+        <article>
+             <div class="image-container">
+             
+
+                <img src="https://api-vehiculos-lj3j.onrender.com/" alt="vehiculo">
+
+            </div>
+
+                    <h2>vehiculo</h2>
+                    <span>estado de personaje</span>
+
+
+        </article>
+        
+    `);
+
+    const main = document.querySelector("main");
+
+    main.append(article);
+
+
+
+    });
+
+
+
 });
